b9cf650c983639e36f535a99d433c3af1a41ccd1
Dowody <54286055+Dowody@users.noreply.github.com>
2022-10-03 22:52:56 +0300

    hello

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ toggle.addEventListener("click", () => {
 const toggleButton = document.getElementsByClassName('hamb')[0]
 const navbarLinks = document.getElementsByClassName('navlinks')[0]
 
+navbarLinks.classList.remove('active')
+
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 });
