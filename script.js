c8fc21e18a82cf501d0578cff71467742009af75
Dowody <54286055+Dowody@users.noreply.github.com>
2022-10-17 20:35:07 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-const toggle = document.querySelector(".moon");
-const bodyy = document.querySelector(".dark");
-
-toggle.addEventListener("click", () => {
-    bodyy.classList.toggle("dark");
-});
+// HAMBURGER MOBILE
 
 const toggleButton = document.getElementsByClassName('hamb')[0]
 const navbarLinks = document.getElementsByClassName('navlinks')[0]
@@ -13,4 +8,7 @@ navbarLinks.classList.remove('active')
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 });
+
+scrollbars.init(document.querySelector("#my-scrollbar"));
+
 
