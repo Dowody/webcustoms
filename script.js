15694a4e93dcccd87e46ed7bea0cf069ee32e5b0
Dowody <54286055+Dowody@users.noreply.github.com>
2022-11-03 17:07:09 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,12 +3,16 @@
 const toggleButton = document.getElementsByClassName('hamb')[0]
 const navbarLinks = document.getElementsByClassName('navlinks')[0]
 
+
+
 navbarLinks.classList.remove('active')
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 });
 
+
 scrollbars.init(document.querySelector("#my-scrollbar"));
+
 
 
