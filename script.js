cd62d1fbb215b8ca07e73aef31eeb61ad4f142cb
Dowody <54286055+Dowody@users.noreply.github.com>
2022-12-22 14:49:33 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,2 @@
-// HAMBURGER MOBILE
-
-const toggleButton = document.getElementsByClassName('hamb')[0]
-const navbarLinks = document.getElementsByClassName('navlinks')[0]
-
-
-
-navbarLinks.classList.remove('active')
-
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-});
-
-
-scrollbars.init(document.querySelector("#my-scrollbar"));
-
 
 
